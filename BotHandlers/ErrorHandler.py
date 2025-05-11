a12f4d7f23c4b431f855b9dd435d8feb2683744e
jxd8y <jxvz@proton.me>
2025-05-11 05:35:07 -0700

    Added traceback to errorHandler

--- BotHandlers/ErrorHandler.py
+++ BotHandlers/ErrorHandler.py
@@ -1,5 +1,6 @@
 from telegram import InlineKeyboardButton,InlineKeyboardMarkup ,ReplyKeyboardMarkup,ReplyKeyboardRemove,Update
 from telegram.ext import Application,filters,CommandHandler,MessageHandler,CallbackQueryHandler,CallbackContext,ConversationHandler
+import traceback
 
 #Following function only handle user interaction based errors
 #-raised by telegram
@@ -8,3 +9,5 @@ async def HandleUserErrorCallback(update:Update, context:CallbackContext):
     await update.effective_chat.send_message("There was an error try again.")
     if update.callback_query is not None:
         await update.callback_query.answer("fail")
+
+    print(traceback.format_exc())
